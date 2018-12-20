e46a314ef99a25001469a061bb79fd01670a649b
Jackie <554586805@qq.com>
2018-12-20 15:47:19 +0800

    first commit

---
+++ song.js
@@ -0,0 +1,46 @@
+$(function(){
+    $.ajax({
+        url:'./lyric.json',
+        type:'GET',
+        dataType:'json'
+    }).done(function(ret){
+        var {lyric}=ret;
+        var array=lyric.split('\n');/*将字符串以'\n'进行分割，分为数组*/
+        array=array.map(function(string){/*map遍历*/
+            var reg=/^\[(.+)\](.*)$/g;
+            var matches=reg.exec(string);/*可以在控制台检验取出来的是什么值*/
+            if(matches){
+                return {time:matches[1],lyric:matches[2]}/*返回一个新数组*/
+            }
+        })
+        array.map(function(object){
+            var $lyric=$('.lyric');
+            var $p=$('<p></p>')
+            $p.attr('time',object.time).text(object.lyric);
+            $p.appendTo($lyric);
+        })
+    })
+    var audio=document.createElement('audio');
+    audio.src='https://m10.music.126.net/20181220155112/fff583f50be2902a0fdfff0bfe039c68/ymusic/3c9e/439c/d816/0f5c0962d2c3067a35bf7163bea08424.mp3';
+    audio.oncanplay=function(){
+        audio.play();
+        $('.light').addClass('playing');
+        $('.cover').addClass('playing');
+        $('.iconbox').addClass('playing1');
+    }
+    $('.cover').on('click',function(){
+        audio.pause();
+        $('.iconbox').removeClass('playing1');
+        $('.light').removeClass('playing');
+        $('.cover').removeClass('playing');
+    })
+    $('.icon').on('click',function(){
+        audio.play();
+        $('.iconbox').addClass('playing1');
+        $('.light').addClass('playing');
+        $('.cover').addClass('playing');
+    })
+  
+})
+
+
---
+++ song1.js
@@ -0,0 +1,5 @@
+var xhr=new XMLHttpRequest();
+xhr.open('get','./lyric.json',false);
+xhr.send();
+var data=xhr.responseText;
+console.log(lyric.split('\n'));
